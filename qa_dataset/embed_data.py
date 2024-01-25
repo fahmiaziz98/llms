2a3ed28cc2627afd55ef94dde2bf7f13329c0d66
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2024-01-25 07:50:42 +0700

    init

--- qa_dataset/embed_data.py
+++ qa_dataset/embed_data.py
@@ -7,20 +7,17 @@ from unstructured.partition.html import partition_html
 from unstructured.cleaners.core import clean, replace_unicode_quotes, clean_non_ascii_chars
 from unstructured.staging.huggingface import chunk_by_attention_window
 
-from transformers import AutoModel, AutoTokenizer
 from src.path import DATA_DIR
 from src.logger import get_console_logger
 from src.vector_db import get_qdrant_client, init_collection
+from src.transformers import MODEL, TOKENIZER
 
 
 NEWS_FILE = DATA_DIR / "financial_news.json"
 QDRANT_COLLECION_NAME = "financial_news"
 QDRANT_VECTOR_SIZE = 384
-MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
 LOGGER = get_console_logger(__name__)
-MODEL = AutoModel.from_pretrained(MODEL_NAME)
-TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 qdrant_client = get_qdrant_client()
 qdrant_client = init_collection(
